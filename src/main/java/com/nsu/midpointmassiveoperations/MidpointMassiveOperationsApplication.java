0cc6b7101431d9fdf050a609ce2cac3129d7fe50
Ivan Sheldyakov <i.sheldyakov@g.nsu.ru>
2023-04-13 21:13:36 +0700

    first commit

---
+++ src/main/java/com/nsu/midpointmassiveoperations/MidpointMassiveOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.nsu.midpointmassiveoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidpointMassiveOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidpointMassiveOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsu/midpointmassiveoperations/MidpointMassiveOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsu.midpointmassiveoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidpointMassiveOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
