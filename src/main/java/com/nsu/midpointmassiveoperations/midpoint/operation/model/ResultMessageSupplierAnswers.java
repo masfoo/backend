5852cabd1be5aad5f0e07288c0fa26c5127a2d02
f0g0 <kirill_k2002@mail.ru>
2023-05-26 16:53:55 +0700

    Added final to class

--- src/main/java/com/nsu/midpointmassiveoperations/midpoint/operation/model/ResultMessageSupplierAnswers.java
+++ src/main/java/com/nsu/midpointmassiveoperations/midpoint/operation/model/ResultMessageSupplierAnswers.java
@@ -1,9 +1,13 @@
 package com.nsu.midpointmassiveoperations.midpoint.operation.model;
 
-public class ResultMessageSupplierAnswers {
-    public static String MIDPOINT_REACH = "Couldn't reach midpoint. ";
-    public static String SUCCESS = "Operation successful. ";
-    public static String USER_NOT_FOUND = "User not found. ";
-    public static String BODY_IS_NULL = "Body is null for query. ";
-    public static String EMPTY_TICKET = "Cannot parse an empty ticket. ";
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ResultMessageSupplierAnswers {
+    public static final String MIDPOINT_REACH = "Couldn't reach midpoint. ";
+    public static final String SUCCESS = "Operation successful. ";
+    public static final String USER_NOT_FOUND = "User not found. ";
+    public static final String BODY_IS_NULL = "Body is null for query. ";
+    public static final String EMPTY_TICKET = "Cannot parse an empty ticket. ";
 }
